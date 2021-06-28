b47d4ec8f63fcff2d72613ab2f2c551e7b96efe4
Dany <danyb-@live.com>
2021-06-28 01:33:07 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'app_widget.dart';
+
+
+void main () {
+  runApp(AppFireBase());
+    }
+  
+
+ class AppFireBase extends StatefulWidget {
+  
+  @override
+  _AppFireBaseState createState() => _AppFireBaseState();
+}
+
+class _AppFireBaseState extends State<AppFireBase> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+ Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Material(
+            child: Center(
+              child: Text(
+                'Não foi possível inicializar o firebase',
+                 textDirection: TextDirection.ltr,
+                 ),
+            ),
+          );
+        }
+        else if (snapshot.connectionState == ConnectionState.done) {
+          return AppWidget();
+        } else {
+          return Material(
+            child: Center(
+              child:CircularProgressIndicator (),
+              ),
+          );
+        }
+      });
+ }
+}
